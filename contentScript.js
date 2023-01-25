42e8983f73ddbfefb8eb5c90da120cd97b7afbc5
Atharv varshney <Atharvvarshney07@gmail.com>
2023-01-25 18:11:42 +0530

    Updated with cleaner version of code

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,7 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
+console.log("content script injected")
 
+
+chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.from === "popup" && message.query === "eye_dropper_clicked") {
 
         setTimeout(() => {
@@ -7,23 +9,18 @@ chrome.runtime.onMessage.addListener((message, sender) => {
             const eyeDropper = new EyeDropper();
 
             eyeDropper.open().then(result => {
-
-                console.log(result);
-
                 chrome.storage.local.get("color_hex_code", (resp) => {
-
                     if (resp.color_hex_code && resp.color_hex_code.length > 0) {
                         chrome.storage.local.set({ "color_hex_code": [...resp.color_hex_code, result.sRGBHex] })
                     }
                     else {
+                        console.log("no")
                         chrome.storage.local.set({ "color_hex_code": [result.sRGBHex] })
                     }
-
                 })
             }).catch(e => {
                 console.log(e)
             })
-
         }, 500);
     }
 })
--- popup.js
+++ popup.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
 
+window.addEventListener('DOMContentLoaded', () => {
     const mainCont = document.getElementById("mainCont");
     const buttonCont = document.getElementById("picker_btn_cont");
     const resultList = document.getElementById("result");
 
+    const GiveMetheChild = (color, msg) => {
+        const errorLabel = document.createElement("p")
+        errorLabel.setAttribute("class", "errorLabel")
+        errorLabel.style.backgroundColor = color
+        errorLabel.innerText = msg
+
+        mainCont.appendChild(errorLabel)
+        setTimeout(() => {
+            mainCont.removeChild(errorLabel)
+        }, 2000)
+    }
 
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         const tab = tabs[0]
@@ -39,37 +50,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
-
     chrome.storage.local.get("color_hex_code", (resp) => {
 
         if (resp.color_hex_code && resp.color_hex_code.length > 0) {
-
             resp.color_hex_code.forEach(hexCode => {
-
                 const liElem = document.createElement("li")
                 liElem.innerText = hexCode
                 liElem.style.backgroundColor = hexCode
-
                 liElem.addEventListener("click", () => {
                     navigator.clipboard.writeText(hexCode);
                     GiveMetheChild("#e19526", "Hex code is copied to clipboard!")
                 })
-
                 resultList.appendChild(liElem)
-
             })
 
             const ClearButton = document.createElement("button")
             ClearButton.innerText = "Clear Memory"
             ClearButton.setAttribute("id", "ClearButton")
-
             ClearButton.addEventListener("click", () => {
                 chrome.storage.local.remove("color_hex_code")
                 window.close()
             })
-
             mainCont.appendChild(ClearButton)
         }
 
